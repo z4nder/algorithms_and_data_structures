71def388a1fdaf324fdc13b021102499f39d3f82
z4nder <gusttavo212@gmail.com>
2023-10-09 09:01:46 -0300

    Finish btree

--- binary-tree/src/main.rs
+++ binary-tree/src/main.rs
@@ -1,14 +1,16 @@
-#[derive(PartialEq, PartialOrd, Debug)]
+use std::fmt::Debug;
+
+#[derive(PartialEq, PartialOrd, Debug, Clone)]
 pub struct BTree<T> {
-    pub value: T,
+    pub value: Option<T>,
     pub left: Option<Box<BTree<T>>>,
     pub right: Option<Box<BTree<T>>>,
 }
 
-impl<T> BTree<T> {
+impl<T: PartialEq + PartialOrd + Clone + Debug> BTree<T> {
     pub fn new(value: T) -> Self {
         BTree {
-            value,
+            value: Some(value),
             left: None,
             right: None,
         }
@@ -23,10 +25,53 @@ impl<T> BTree<T> {
         self.right = Some(Box::new(node));
         self
     }
+
+    pub fn insert(&mut self, value: T) -> &Self {
+        match &self.value {
+            Some(current) => loop {
+                if value < *current {
+                    match &self.left {
+                        Some(left_node_value) => {
+                            let mut left_node = left_node_value.clone();
+                            let left = left_node.insert(value.clone()).clone();
+                            self.left = Some(Box::new(left));
+                            return self;
+                        }
+                        None => {
+                            self.left = Some(Box::new(Self::new(value.clone())));
+                            return self;
+                        }
+                    }
+                } else {
+                    match &self.right {
+                        Some(right_node_value) => {
+                            let mut right_node = right_node_value.clone();
+                            let right = right_node.insert(value.clone()).clone();
+                            self.right = Some(Box::new(right));
+                        }
+                        None => {
+                            self.right = Some(Box::new(Self::new(value.clone())));
+                            return self;
+                        }
+                    }
+                }
+
+                return self;
+            },
+            None => {
+                self.value = Some(value.clone());
+                return self;
+            }
+        }
+    }
 }
 
 fn main() {
-    let tree = BTree::new(1).left(BTree::new(2)).right(BTree::new(3));
+    let mut tree = BTree::new(4);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(2);
+    tree.insert(17);
 
     dbg!(tree);
 }
